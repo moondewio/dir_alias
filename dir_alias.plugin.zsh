bb17d67c04821f4e163651d951721add2dd9d389
gillchristian <gillchristiang@gmail.com>
2018-11-09 18:43:09 +0700

    dir_alias.zsh -> dir_alias.plugin.zsh

--- dir_alias.plugin.zsh
+++ dir_alias.plugin.zsh
@@ -1,4 +1,65 @@
-source ./dir_alias.zsh
+# $1 -> file
+# $2 -> method
+function processAliasesFile () {
+    while read line; do
+        # ignore empty lines and comments
+        if [[  -z $line  ]] || [[  $line == \#*  ]]; then
+            continue
+        fi
+
+        # item after ' ' and before '='
+        aliasName=$(echo "$line" | cut -d' ' -f2 | cut -d'=' -f1)
+        # all items after '='
+        aliasContent=$(echo "$line" | cut -d'=' -f2-)
+
+        if [[ $aliasContent == *\; ]]; then
+          aliasContent=${aliasContent/\;/}
+        fi
+
+        # aliased commands might be strings
+        if [[ $aliasContent == \"* ]]; then
+          aliasContent=${aliasContent//\"/}
+        elif [[ $aliasContent == \'* ]]; then
+          aliasContent=${aliasContent//\'/}
+        fi
+
+
+        "$2" "$aliasName" "$aliasContent"
+    done < "$1"
+}
+
+# $1 -> alias name
+# $2 -> alias content
+function destroyAlias () {
+    alias "$1" 2> /dev/null > /dev/null && unalias "$1"
+}
+
+# $1 -> alias name
+# $2 -> alias content
+function createAlias () {
+    alias "$1"="$2"
+}
+
+# $1 -> directory
+# $2 -> method
+function applyMethodToAliasesFiles () {
+    if [[ -z $1 ]]; then
+        return 0
+    fi
+
+    if [ -f "$1/.aliasfile" ]; then
+        processAliasesFile "$1/.aliasfile" "$2"
+    fi
+
+    if [ "$1" != "/" ]; then
+        applyMethodToAliasesFiles "$(dirname "$1")" "$2"
+    fi
+}
+
+function dir_alias () {
+    applyMethodToAliasesFiles "$OLDPWD" destroyAlias
+    applyMethodToAliasesFiles $(pwd) createAlias
+}
 
 add-zsh-hook chpwd dir_alias;
 dir_alias;
